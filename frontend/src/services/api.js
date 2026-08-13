390c91e06e771ca6241f952cfa6cd28cc9d1fbe1
HemaKalyani45 <hemakalyani56@gmail.com>
2026-08-13 09:09:14 +0530

    changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://neuzen-hrms-o1zj.vercel.app/',
   headers: {
     'Content-Type': 'application/json'
   }
