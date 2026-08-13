485e59ed716c3ec24af676eca60a20515680c12b
HemaKalyani45 <hemakalyani56@gmail.com>
2026-08-13 13:03:06 +0530

    update api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,13 +2,13 @@ import axios from 'axios';
 
 const api = axios.create({
  
-   baseURL:'http://localhost:5000/api/'
-   //   baseURL: "https://neuzen-hrms-vcav.vercel.app/api",
+  // baseURL:'http://localhost:5000/api/'
+      baseURL: "https://neuzen-hrms-vcav.vercel.app/api/",
 
   
-  // headers: {
-  //   'Content-Type': 'application/json'
-  // }
+   headers: {
+     'Content-Type': 'application/json'
+   }
 });
 
 api.interceptors.request.use((config) => {
